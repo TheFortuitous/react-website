49d9efcfd7403f6c9aef22974a96845061f38a3b
Aleksander <54500854+TheFortuitous@users.noreply.github.com>
2023-01-07 19:54:13 +0100

    first commit

---
+++ Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Main() {
+    return (
+      <div>
+        <h1>Hello, World!</h1>
+        <p>This is a basic React component.</p>
+      </div>
+    );
+  }
+  
+  export default Main;
